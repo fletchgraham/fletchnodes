b17fdcc140a14246d7beac886f9c0d90a1c1685a
fletchgraham <sfletchgraham@gmail.com>
2023-01-22 15:07:36 -0500

    chore: move addon files into folder. make init for addon

---
+++ fletchnodes/__init__.py
@@ -0,0 +1,42 @@
+bl_info = {
+    "name": "fletchnodes",
+    "author": "Fletcher Graham",
+    "version": (0, 1, 0),
+    "blender": (3, 4, 0),
+    "description": "Node groups for Geometry Nodes.",
+    "category": "Node",
+}
+
+import bpy
+
+
+class ObjectMoveX(bpy.types.Operator):
+    """My Object Moving Script"""      # Use this as a tooltip for menu items and buttons.
+    bl_idname = "object.move_x"        # Unique identifier for buttons and menu items to reference.
+    bl_label = "Move X by One"         # Display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+
+    def execute(self, context):        # execute() is called when running the operator.
+
+        # The original script
+        scene = context.scene
+        for obj in scene.objects:
+            obj.location.x += 1.0
+
+        return {'FINISHED'}            # Lets Blender know the operator finished successfully.
+
+def menu_func(self, context):
+    self.layout.operator(ObjectMoveX.bl_idname)
+
+def register():
+    bpy.utils.register_class(ObjectMoveX)
+    bpy.types.VIEW3D_MT_object.append(menu_func)  # Adds the new operator to an existing menu.
+
+def unregister():
+    bpy.utils.unregister_class(ObjectMoveX)
+
+
+# This allows you to run the script directly from Blender's Text editor
+# to test the add-on without having to install it.
+if __name__ == "__main__":
+    register()
